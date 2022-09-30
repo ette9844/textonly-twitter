7c8611684642bda0c6c1070988f710d200ed807e
ette9844 <ette9844@gmail.com>
2022-09-30 11:18:41 +0900

    add hide emoji setting

--- contentScripts/script.js
+++ contentScripts/script.js
@@ -3,9 +3,10 @@ const $body = $('body');
 // Initialize
 let setting = {};
 
-chrome.storage.sync.get(['textonly', 'grayscaleEmoji', 'mediaSize'], function(result) {
+chrome.storage.sync.get(['textonly', 'grayscaleEmoji', 'hideEmoji', 'mediaSize'], function(result) {
     setting.textonly = result.textonly != null ? result.textonly : true;
     setting.grayscaleEmoji = result.grayscaleEmoji != null ? result.grayscaleEmoji : true;
+    setting.hideEmoji = result.hideEmoji != null ? result.hideEmoji : true;
     setting.mediaSize = result.mediaSize != null ? result.mediaSize : 100;
     renderAll(setting);
 });
@@ -27,6 +28,14 @@ function renderGrayscaleEmoji(grayscaleEmoji) {
     }
 }
 
+function renderHideEmoji(hideEmoji) {
+    if (hideEmoji) {
+        $body.addClass('tt-hide-emoji');
+    } else {
+        $body.removeClass('tt-hide-emoji');
+    }
+}
+
 function renderMediaSize(mediaSize) {
     $body.removeClass('tt-media-size-100 tt-media-size-75 tt-media-size-50 tt-media-size-25 tt-media-size-0');
     $body.addClass('tt-media-size-' + mediaSize);
@@ -35,6 +44,7 @@ function renderMediaSize(mediaSize) {
 function renderAll(setting) {
     renderTextonly(setting.textonly);
     renderGrayscaleEmoji(setting.grayscaleEmoji);
+    renderHideEmoji(setting.hideEmoji);
     renderMediaSize(setting.mediaSize);
 }
 
@@ -51,6 +61,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             break;
         case 'RENDER_GRAYSCALE_EMOJI':
             renderGrayscaleEmoji(setting.grayscaleEmoji);
+            break;
+        case 'RENDER_HIDE_EMOJI':
+            renderHideEmoji(setting.hideEmoji);
             break;
         case 'RENDER_MEDIA_SIZE':
             renderMediaSize(setting.mediaSize);
--- popup/popup.js
+++ popup/popup.js
@@ -3,17 +3,20 @@ $(function() {
     let setting = {};
     const $textonlyInput = $('#textonly-input');
     const $grayscaleEmojiInput = $('#grayscale-emoji-input');
+    const $hideEmojiInput = $('#hide-emoji-input');
     const $mediaSizeSelect = $('#media-size-select');
     const $whitebox = $(".whitebox");
 
-    chrome.storage.sync.get(['textonly', 'grayscaleEmoji', 'mediaSize'], function(result) {
+    chrome.storage.sync.get(['textonly', 'grayscaleEmoji', 'hideEmoji', 'mediaSize'], function(result) {
         setting.textonly = result.textonly != null ? result.textonly : true;
         setting.grayscaleEmoji = result.grayscaleEmoji != null ? result.grayscaleEmoji : true;
+        setting.hideEmoji = result.hideEmoji != null ? result.hideEmoji : true;
         setting.mediaSize = result.mediaSize != null ? result.mediaSize : 100;
 
         setting.textonly ? $whitebox.hide() : $whitebox.show();
         $textonlyInput.prop('checked', setting.textonly);
         $grayscaleEmojiInput.prop('checked', setting.grayscaleEmoji);
+        $hideEmojiInput.prop('checked', setting.hideEmoji);
         $mediaSizeSelect.val(setting.mediaSize);
     });
 
@@ -22,22 +25,17 @@ $(function() {
         setting.textonly = this.checked ? true : false;
         setting.textonly ? $whitebox.hide() : $whitebox.show();
         chrome.storage.sync.set({ textonly: setting.textonly });
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { message: 'RENDER_TEXTONLY', setting: setting });
-        });
     });
     $grayscaleEmojiInput.on('change', function() {
         setting.grayscaleEmoji = this.checked ? true : false;
         chrome.storage.sync.set({ grayscaleEmoji: setting.grayscaleEmoji });
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { message: 'RENDER_GRAYSCALE_EMOJI', setting: setting });
-        });
+    });
+    $hideEmojiInput.on('change', function() {
+        setting.hideEmoji = this.checked ? true : false;
+        chrome.storage.sync.set({ hideEmoji: setting.hideEmoji });
     });
     $mediaSizeSelect.on('change', function() {
         setting.mediaSize = $(this).val();
         chrome.storage.sync.set({ mediaSize: setting.mediaSize });
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { message: 'RENDER_MEDIA_SIZE', setting: setting });
-        });
     });
 });
